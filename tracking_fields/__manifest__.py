e1b2f9115ff76580bf8c8dd94c62c401f860f3df
Fortich <ajsuarezf@unal.edu.co>
2020-08-20 16:21:45 -0500

    Updating version

--- tracking_fields/__manifest__.py
+++ tracking_fields/__manifest__.py
@@ -10,14 +10,14 @@
         Module to add tracking to base fields.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xphera",
+    'website': "http://xphera.co",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '13.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
