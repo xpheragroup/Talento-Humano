d633707809f9d23a3232bd0bb82124e22545edc1
Cizaquita <cristian.izaquita@gmail.com>
2021-05-20 16:01:24 -0500

    Mod: create for journal overwrite def.

--- overwrite_accounting/models/account.py
+++ overwrite_accounting/models/account.py
@@ -40,6 +40,58 @@ class AccountJournal(models.Model):
                 'company_id': company.id,
         }  # Short code is needed larger (size was 5, set 10)
 
+
+    def create(self, vals):
+        company_id = vals.get('company_id', self.env.company.id)
+        if vals.get('type') in ('bank', 'cash'):
+            # For convenience, the name can be inferred from account number
+            if not vals.get('name') and 'bank_acc_number' in vals:
+                vals['name'] = vals['bank_acc_number']
+
+            # If no code provided, loop to find next available journal code
+            if not vals.get('code'):
+                vals['code'] = self.get_next_bank_cash_default_code(vals['type'], company_id)
+                if not vals['code']:
+                    raise UserError(_("Cannot generate an unused journal code. Please fill the 'Shortcode' field."))
+
+
+            # Create a default debit/credit account if not given
+            default_account = vals.get('default_debit_account_id') or vals.get('default_credit_account_id')
+            company = self.env['res.company'].browse(company_id)
+            if not default_account:
+                account_vals = self._prepare_liquidity_account(vals.get('name'), company, vals.get('currency_id'), vals.get('type'))
+
+                if vals.get('name') != "Efectivo":
+                    
+                    default_account = self.env['account.account'].create(account_vals)
+                    vals['default_debit_account_id'] = default_account.id
+                    vals['default_credit_account_id'] = default_account.id
+
+
+            if vals['type'] == 'cash':
+                if not vals.get('profit_account_id'):
+                    vals['profit_account_id'] = company.default_cash_difference_income_account_id.id
+                if not vals.get('loss_account_id'):
+                    vals['loss_account_id'] = company.default_cash_difference_expense_account_id.id
+
+        if 'refund_sequence' not in vals:
+            vals['refund_sequence'] = vals['type'] in ('sale', 'purchase')
+
+        # We just need to create the relevant sequences according to the chosen options
+        if not vals.get('sequence_id'):
+            vals.update({'sequence_id': self.sudo()._create_sequence(vals).id})
+        if vals.get('type') in ('sale', 'purchase') and vals.get('refund_sequence') and not vals.get('refund_sequence_id'):
+            vals.update({'refund_sequence_id': self.sudo()._create_sequence(vals, refund=True).id})
+        journal = super(AccountJournal, self.with_context(mail_create_nolog=True)).create(vals)
+        if 'alias_name' in vals:
+            journal._update_mail_alias(vals)
+
+        # Create the bank_account_id if necessary
+        if journal.type == 'bank' and not journal.bank_account_id and vals.get('bank_acc_number'):
+            journal.set_bank_account(vals.get('bank_acc_number'), vals.get('bank_id'))
+
+        return journal
+
 class AccountAccount(models.Model):
     _inherit = "account.account"
 
@@ -98,7 +150,6 @@ class AccountChartTemplate(models.Model):
         # Create the journals that will trigger the account.account creation
         for acc in self._get_default_bank_journals_data():
             bank_journals += self.env['account.journal'].create({
-                'id': acc['acc_name'] + '_01',
                 'name': acc['acc_name'],
                 'type': acc['account_type'],
                 'company_id': company.id,
